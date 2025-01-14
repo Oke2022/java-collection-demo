9dc7c72fcb9e95ca2c830d9066a744e2c7e6336e
okejoshua <okejoshua391@gmail.com>
2025-01-13 23:17:21 -0500

    removed all comments

--- src/main/java/com/example/servlets/StageServlet.java
+++ src/main/java/com/example/servlets/StageServlet.java
@@ -3,13 +3,10 @@ package com.example.servlets;
 import com.example.models.*;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//@WebServlet("/")
+
 public class StageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
